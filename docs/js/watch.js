3132230513a697fec94ac247b4a6662a8d71e908
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-02-06 13:32:18 +0530

    added docs folder

---
+++ docs/js/watch.js
@@ -0,0 +1,53 @@
+const startBtn = document.getElementById('start-share');
+const stopBtn = document.getElementById('stop-share');
+const videoGrid = document.getElementById('watch-video-grid');
+const video = document.createElement('video');
+video.muted = true;
+
+let myId;
+
+const socket = io();
+
+var peer = new Peer();
+
+var conn = peer.connect();
+
+document.getElementById('myid').innerHTML = '123';
+
+peer.on('open', function (id) {
+	console.log('My peer ID is: ' + id);
+	myId = id;
+	socket.emit('join', { roomId: 'ScreenShare', user: id });
+});
+
+peer.on('call', function (call) {
+	// Answer the call, providing our mediaStream
+	call.answer();
+
+	call.on('stream', function (stream) {
+		video.srcObject = stream;
+
+		video.style.width = '100%';
+		video.style.height = '100%';
+		video.controls = true;
+
+		video.addEventListener('loadedmetadata', () => {
+			video.play();
+		});
+
+		videoGrid.append(video);
+
+		stream.getVideoTracks()[0].onended = function () {
+			stopScreen(stream);
+		};
+	});
+});
+
+function stopScreen(stream) {
+	video.srcObject = null;
+	videoGrid.innerHTML = null;
+	stream.getTracks().forEach((track) => track.stop());
+
+	startBtn.disabled = false;
+	stopBtn.disabled = true;
+}
